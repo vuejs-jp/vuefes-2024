65b95caaf769caa402a6a4b861d4744d665c497b
KannoStanfoot <kanno@stanfoot.com>
2024-02-10 21:11:40 +0900

    :style change double quotation to single quotation

--- apps/web/nuxt.config.ts
+++ apps/web/nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  srcDir: "app/",
-  modules: ["@vuejs-jp/vuefes-ui", "@nuxt/content"],
+  srcDir: 'app/',
+  modules: ['@vuejs-jp/vuefes-ui', '@nuxt/content'],
   devtools: { enabled: true },
 });
