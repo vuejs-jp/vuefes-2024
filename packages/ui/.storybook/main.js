4c6dd444953ba3003d738805d873514b023ad208
jiyuujin <jiyuujinunite@gmail.com>
2024-02-13 20:51:08 +0900

    feat: chromatic

--- packages/ui/.storybook/main.js
+++ packages/ui/.storybook/main.js
@@ -1,3 +1,6 @@
+import { mergeConfig } from 'vite'
+import turbosnap from 'vite-plugin-turbosnap'
+
 module.exports = {
   stories: ['../components/**/*.stories.mdx', '../components/**/*.stories.@(js|ts)'],
   addons: [
@@ -13,5 +16,13 @@ module.exports = {
         viteConfigPath: '.storybook/vite.config.ts',
       },
     },
+  },
+  async viteFinal(config, { configType }) {
+    return mergeConfig(config, {
+      plugins:
+        configType === 'PRODUCTION'
+          ? [turbosnap({ rootDir: config.root ?? process.cwd() })]
+          : [],
+    })
   },
 }
