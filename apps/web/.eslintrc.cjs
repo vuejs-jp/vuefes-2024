455894fbed98c513ff3cec711500c21e2eecb9a5
ryuhei373 <mail@ryuhei.dev>
2024-03-02 20:43:22 +0900

    add prettierrc and eslint rules

--- apps/web/.eslintrc.cjs
+++ apps/web/.eslintrc.cjs
@@ -9,10 +9,36 @@ module.exports = {
     'vue/no-v-for-template-key-on-child': 'off',
     'vue/no-deprecated-slot-attribute': 'off',
     'vue/singleline-html-element-content-newline': 'off',
+    'vue/multiline-html-element-content-newline': 'off',
+    'vue/max-attributes-per-line': 'off',
     'vue/multi-word-component-names': [
       'error',
       {
-        ignores: ['index', 'default', 'error', 'Example'],
+        ignores: [
+          'index',
+          'default',
+          'error',
+          'privacy',
+          'register',
+          'users',
+          'events',
+          'Provider',
+          '[id]',
+        ],
+      },
+    ],
+    'vue/html-closing-bracket-newline': 'off',
+    'vue/html-indent': 'off',
+    'vue/html-self-closing': [
+      'error',
+      {
+        html: {
+          void: 'always',
+          normal: 'always',
+          component: 'always',
+        },
+        svg: 'always',
+        math: 'always',
       },
     ],
   },
