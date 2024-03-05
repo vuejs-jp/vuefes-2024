e67f0aada992beabc4a92e051bb07ba4ff5e4bb5
jiyuujin <jiyuujinunite@gmail.com>
2024-03-05 12:30:26 +0900

    add base config

---
+++ packages/eslint-config/vue-library.js
@@ -0,0 +1,25 @@
+module.exports = {
+  extends: ['./base.js'],
+  parser: 'vue-eslint-parser',
+  rules: {
+    'vue/no-v-for-template-key-on-child': 'off',
+    'vue/no-deprecated-slot-attribute': 'off',
+    'vue/singleline-html-element-content-newline': 'off',
+    'vue/multiline-html-element-content-newline': 'off',
+    'vue/max-attributes-per-line': 'off',
+    'vue/html-closing-bracket-newline': 'off',
+    'vue/html-indent': 'off',
+    'vue/html-self-closing': [
+      'error',
+      {
+        html: {
+          void: 'always',
+          normal: 'always',
+          component: 'always',
+        },
+        svg: 'always',
+        math: 'always',
+      },
+    ],
+  },
+}
