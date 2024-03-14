988e04fbab1b8407ca01acb4812faad615597add
kokoakuma <aikw9101@gmail.com>
2024-03-14 14:13:20 +0000

    show md text

--- apps/web/app/pages/privacy.vue
+++ apps/web/app/pages/privacy.vue
@@ -1,1 +1,21 @@
-<template>PrivacyPolicy</template>
+<script setup lang="ts">
+import FooterPageSection from '~/components/FooterPageSection.vue'
+import MarkDownText from '~/components/MarkDownText.vue'
+</script>
+
+<template>
+  <main>
+    <NavPageSection />
+    <div class="privacy-root">
+      <MarkDownText path="privacy" class="explain" />
+
+      <div class="back">
+        <button to="/" outline>Button</button>
+      </div>
+    </div>
+    <FooterPageSection />
+  </main>
+</template>
+
+<style scoped>
+</style>
