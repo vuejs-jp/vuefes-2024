e2f46cbbcc392041d2e0d6fa79c19d013106fd9b
totocalcio <totohyper@yahoo.co.jp>
2024-03-14 23:31:23 +0900

    feat: add TextLink component

---
+++ packages/ui/components/TextLink.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import type { Color } from '@vuejs-jp/model'
+import { useColor } from '@vuejs-jp/composable'
+import { AnchorHTMLAttributes } from 'vue'
+
+interface TextLinkProps extends /* @vue-ignore */ AnchorHTMLAttributes {
+  /* Semantic Color */
+  color: Color
+}
+const props = defineProps<TextLinkProps>()
+
+const { color: textColor } = useColor()
+</script>
+
+<template>
+  <a :href :target :style="{ color: textColor(props.color) }" class="text-link">
+    <slot />
+  </a>
+</template>
+
+<style scoped>
+/* TODO デザイン適用 */
+.text-link {
+  text-decoration: underline;
+  cursor: pointer;
+}
+</style>
