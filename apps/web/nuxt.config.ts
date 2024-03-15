af9b05be2c341ef45be09bdb334351da544497ac
toshick <toshickjazz@mac.com>
2024-03-15 22:00:14 +0900

    feat: change base url to /2024

--- apps/web/nuxt.config.ts
+++ apps/web/nuxt.config.ts
@@ -2,6 +2,9 @@ import svgLoader from 'vite-svg-loader'
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    baseURL: '/2024',
+  },
   srcDir: 'app/',
   modules: ['@vuejs-jp/vuefes-ui', '@nuxtjs/i18n', '@nuxt/content'],
   i18n: {
