46bd5951cdc6e59837276a6c67a8a21425485dd3
jiyuujin <jiyuujinunite@gmail.com>
2024-04-26 17:52:02 +0900

    apply vue-green/200

--- packages/composable/lib/useColor.ts
+++ packages/composable/lib/useColor.ts
@@ -7,6 +7,7 @@ export function useColor() {
       .with('white', () => '#ffffff')
       .with('vue-blue', () => '#35495e')
       .with('vue-green', () => '#42b883')
+      .with('vue-green/100', () => '#42B883')
       .with('vue-green/200', () => '#208558')
       .with('asagi/100', () => '#33A6B8')
       .with('asagi/200', () => '#1A8191')
