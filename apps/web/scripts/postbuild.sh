542e7eae36820bf46b02fa41c2092d1641b1406a
jiyuujin <jiyuujinunite@gmail.com>
2024-04-27 17:57:01 +0900

    update scripts

--- apps/web/scripts/postbuild.sh
+++ apps/web/scripts/postbuild.sh
@@ -4,13 +4,15 @@
 # change dir structure
 # ./dist/{html} -> ./dist/2024/{html}
 # 
-if [ ! -d "./dist" ]; then
-    echo "can not find dist"
+if [ ! -d "./.output" ]; then
+    echo "can not find .output"
     exit 1;
 fi
 
+mkdir ./dist
+
 mkdir ./tmp
-mv ./dist/* ./tmp
+mv ./.output/* ./tmp
 mkdir ./dist/2024
 mv ./tmp/* ./dist/2024
 rm -rf ./tmp
