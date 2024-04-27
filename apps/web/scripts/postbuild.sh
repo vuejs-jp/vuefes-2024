86ca117404a529ad851126c59c504a1982440203
jiyuujin <jiyuujinunite@gmail.com>
2024-04-27 17:33:35 +0900

    fix: dist

--- apps/web/scripts/postbuild.sh
+++ apps/web/scripts/postbuild.sh
@@ -4,13 +4,14 @@
 # change dir structure
 # ./dist/{html} -> ./dist/2024/{html}
 # 
+mkdir ./dist
 if [ ! -d "./dist" ]; then
     echo "can not find dist"
     exit 1;
 fi
 
 mkdir ./tmp
-mv ./dist/* ./tmp
+mv ./.output/* ./tmp
 mkdir ./dist/2024
 mv ./tmp/* ./dist/2024
 rm -rf ./tmp
