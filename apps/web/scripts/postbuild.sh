057fc5b05aabb675ff1ddfffad3eb50260c1fded
jiyuujin <jiyuujinunite@gmail.com>
2024-04-27 17:15:07 +0900

    hotfix

--- apps/web/scripts/postbuild.sh
+++ apps/web/scripts/postbuild.sh
@@ -4,13 +4,13 @@
 # change dir structure
 # ./dist/{html} -> ./dist/2024/{html}
 # 
-# if [ ! -d "./.output" ]; then
-#     echo "can not find .output"
+# if [ ! -d "./dist" ]; then
+#     echo "can not find dist"
 #     exit 1;
 # fi
 
 mkdir ./tmp
-mv ./.output/* ./tmp
+mv ./.nuxt/dist/* ./tmp
 mkdir ./dist
 mkdir ./dist/2024
 mv ./tmp/* ./dist/2024
