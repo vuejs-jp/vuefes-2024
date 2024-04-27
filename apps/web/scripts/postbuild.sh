115e81011a82429d8014ca245ef9d56b9aa5efdd
jiyuujin <jiyuujinunite@gmail.com>
2024-04-27 17:02:45 +0900

    test - fix: switch deploy

--- apps/web/scripts/postbuild.sh
+++ apps/web/scripts/postbuild.sh
@@ -4,13 +4,14 @@
 # change dir structure
 # ./dist/{html} -> ./dist/2024/{html}
 # 
-if [ ! -d "./dist" ]; then
-    echo "can not find dist"
+if [ ! -d "./.output" ]; then
+    echo "can not find .output"
     exit 1;
 fi
 
 mkdir ./tmp
-mv ./dist/* ./tmp
+mv ./.output/* ./tmp
+mkdir ./dist
 mkdir ./dist/2024
 mv ./tmp/* ./dist/2024
 rm -rf ./tmp
