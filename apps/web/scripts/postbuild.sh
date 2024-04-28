84cc63a246501aec57344a31a90a8a1b4af0d782
jiyuujin <jiyuujinunite@gmail.com>
2024-04-28 21:39:56 +0900

    fix: path

--- apps/web/scripts/postbuild.sh
+++ apps/web/scripts/postbuild.sh
@@ -4,13 +4,13 @@
 # change dir structure
 # ./dist/{html} -> ./dist/2024/{html}
 # 
-if [ ! -d "./.nuxt/dist" ]; then
+if [ ! -d "./dist" ]; then
     echo "can not find dist"
     exit 1;
 fi
 
 mkdir ./tmp
-mv ./.nuxt/dist/* ./tmp
+mv ./dist/* ./tmp
 mkdir ./dist/2024
 mv ./tmp/* ./dist/2024
 rm -rf ./tmp
