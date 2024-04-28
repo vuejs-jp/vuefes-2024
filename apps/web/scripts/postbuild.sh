63ff61ec769ee52758adb7161ce7a5531a5a4a56
jiyuujin <jiyuujinunite@gmail.com>
2024-04-28 13:02:21 +0900

    ssr test

--- apps/web/scripts/postbuild.sh
+++ apps/web/scripts/postbuild.sh
@@ -4,13 +4,13 @@
 # change dir structure
 # ./dist/{html} -> ./dist/2024/{html}
 # 
-if [ ! -d "./dist" ]; then
+if [ ! -d "./.nuxt/dist" ]; then
     echo "can not find dist"
     exit 1;
 fi
 
 mkdir ./tmp
-mv ./dist/* ./tmp
+mv ./.nuxt/dist/* ./tmp
 mkdir ./dist/2024
 mv ./tmp/* ./dist/2024
 rm -rf ./tmp
