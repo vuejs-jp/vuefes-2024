ebc9d1c62e867bfa1e4764a93373ae3659744f58
toshick <toshickjazz@mac.com>
2024-05-21 23:37:09 +0900

    fix: remove speaker_ prefix

--- supabase/schema.sql
+++ supabase/schema.sql
@@ -1,5 +1,10 @@
 create table if not exists public.speakers (
   id uuid not null primary key default uuid_generate_v4(),
+  name varchar(100) not null,
+  caption varchar(100) not null,
+  description text not null,
+  github_url varchar(100) not null,
+  x_url varchar(100) not null,
   session_title varchar(100) not null,
   session_description text not null,
   session_comment varchar(100) not null,
@@ -8,11 +13,6 @@ create table if not exists public.speakers (
   session_time_duration int(10) not null,
   session_doc_title varchar(100) not null,
   session_doc_url varchar(100) not null,
-  speaker_name varchar(100) not null,
-  speaker_caption varchar(100) not null,
-  speaker_description text not null,
-  speaker_github_url varchar(100) not null,
-  speaker_x_url varchar(100) not null,
   created_at timestamp with time zone default timezone('utc' :: text, now()) not null,
   updated_at timestamp with time zone default timezone('utc' :: text, now()) not null
 );
