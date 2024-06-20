a63c0d09b4ee75ce29fd98b2fc2e4b0caf5c6af8
jiyuujin <jiyuujinunite@gmail.com>
2024-06-20 22:17:42 +0900

    Dialog

---
+++ packages/ui/components/common/Dialog.stories.ts
@@ -0,0 +1,35 @@
+import { StoryFn } from '@storybook/vue3'
+import Dialog from './Dialog.vue'
+
+export default {
+  title: 'common/Dialog',
+  component: Dialog,
+  args: {
+    default: '<strong>Go!</strong>',
+  },
+  argTypes: {
+    default: {
+      description: 'The default Vue slot',
+      control: {
+        type: 'text',
+      },
+      table: {
+        category: 'Slots',
+        type: {
+          summary: 'html',
+        },
+      },
+    },
+  },
+}
+
+const Template: StoryFn<unknown> = (args, { argTypes }) => ({
+  props: Object.keys(argTypes),
+  components: { Dialog },
+  setup() {
+    return { args }
+  },
+  template: '<Dialog v-bind="args">{{ args.default }}</Dialog>',
+})
+
+export const Default = Template.bind({})
---
+++ packages/ui/components/common/Dialog.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="mask" @click="$emit('on-close')">
+    <div class="root" role="document" @click="(e) => e.stopPropagation()">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.root {
+  position: absolute;
+  z-index: 9999;
+  left: 50%;
+  top: 50%;
+  transform: translateX(-50%) translateY(-50%);
+  width: 80%;
+  max-width: 960px;
+  transition: .2s;
+  background-color: #fff;
+}
+.mask {
+  position: fixed;
+  z-index: 9999;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  background-color: rgba(0, 0, 0, 0.5);
+}
+</style>
