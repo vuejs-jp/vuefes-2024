618e6ba3a5cc96cbfca8ae928a5e1f098be5d6db
jiyuujin <jiyuujinunite@gmail.com>
2024-06-21 22:35:02 +0900

    refactor: table name

--- apps/web/app/composables/useSupabase.ts
+++ apps/web/app/composables/useSupabase.ts
@@ -1,16 +1,16 @@
 import { useSupabaseClient } from '#imports'
-import { bucket } from '@vuejs-jp/model'
+import { bucket, type Table } from '@vuejs-jp/model'
 import type { Database } from '~/types/generated/supabase'
 import type { FormSpeaker } from '~/types/supabase'
 
 export function useSupabase() {
   const client = useSupabaseClient<Database>()
 
-  async function fetchData(table: 'speakers') {
+  async function fetchData(table: Table) {
     return await client.from(table).select()
   }
 
-  async function upsertSpeaker(table: 'speakers', target: FormSpeaker) {
+  async function upsertSpeaker(table: Table, target: FormSpeaker) {
     const targetData = { ...target }
 
     const { error } = await client.from(table).upsert(targetData)
