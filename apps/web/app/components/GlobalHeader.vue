3f8ce05efd85f13264edf465f564be55fd760348
totocalcio <totohyper@yahoo.co.jp>
2024-06-30 14:34:09 +0900

    fix locale switch dom

--- apps/web/app/components/GlobalHeader.vue
+++ apps/web/app/components/GlobalHeader.vue
@@ -4,17 +4,15 @@ const { isMobile } = useDevice()
 const { locale } = useI18n({ useScope: 'global' })
 const switchLocalePath = useSwitchLocalePath()
 const config = useRuntimeConfig()
+
+const onSwitchLocalePath = () => {
+  switchLocalePath(locale.value === 'ja' ? 'en' : 'ja')
+}
 </script>
 
 <template>
   <VFSpHeader v-if="isMobile">
-    <NuxtLink
-      v-if="config.public.enableSwitchLocale"
-      :to="switchLocalePath(locale === 'ja' ? 'en' : 'ja')"
-      class="locale-switch-wrapper"
-    >
-      <VFLocaleSwitch :locale />
-    </NuxtLink>
+    <VFLocaleSwitch :locale @switch-locale-path="onSwitchLocalePath" />
   </VFSpHeader>
   <VFHeader v-if="!isMobile">
     <NuxtLink
