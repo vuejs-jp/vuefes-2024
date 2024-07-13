0888b51452b64bf9df66d9c204d9c2d74032ce54
KannoStanfoot <kanno@stanfoot.com>
2024-07-14 01:42:53 +0900

    feat(auth): add middleware for session validation

--- apps/web/app/composables/useAuth.ts
+++ apps/web/app/composables/useAuth.ts
@@ -1,11 +1,11 @@
 import { useSupabaseClient } from '#imports'
-import type { AuthProvider, RedirectPath } from '@vuejs-jp/model'
+import type { AuthProvider } from '@vuejs-jp/model'
 import { REDIRECT_URL } from '~/utils/environment.constants'
 
 export function useAuth() {
   const supabase = useSupabaseClient()
 
-  async function signIn(provider: Extract<AuthProvider, 'github' | 'google'>, path: RedirectPath) {
+  async function signIn(provider: Extract<AuthProvider, 'github' | 'google'>, path: string) {
     const { error } = await supabase.auth.signInWithOAuth({
       provider,
       options: {
@@ -22,5 +22,17 @@ export function useAuth() {
     }
   }
 
-  return { signIn, signOut }
+  async function getUser() {
+    const { data,error } = await supabase.auth.getUser()
+    if (error) {
+      throw new Error('can not get user')
+    }
+   return data.user
+  }
+
+  const authUserId = getUser().then(user => {
+    return user.id
+  }).catch(()=> null)
+
+  return { signIn, signOut, authUserId }
 }
