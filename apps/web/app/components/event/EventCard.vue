8fae375b2b141f43b79a51d3dfbf4b021bc0c6ea
jiyuujin <jiyuujinunite@gmail.com>
2024-07-16 18:23:35 +0900

    EventPageSection (WIP)

--- apps/web/app/components/event/EventCard.vue
+++ apps/web/app/components/event/EventCard.vue
@@ -11,10 +11,32 @@ const props = defineProps<EventCardProps>()
 <template>
   <VFEventFrame :title="$t(`event.${title.replaceAll('-', '_')}`)">
     <template #content>
-      <MarkdownText :path="title.replaceAll('-', '_')" />
+      <div class="eventcard-content">
+        <MarkDownText :path="title.replaceAll('-', '_')" />
+      </div>
     </template>
     <template #default>
-      Test
+      <slot />
     </template>
   </VFEventFrame>
 </template>
+
+<style scoped>
+.eventcard-content {
+  font-size: 16px;
+  font-weight: 500;
+  line-height: 25.6px;
+  color: var(--color-vue-blue);
+}
+
+.eventcard-content ::v-deep(a) {
+  color: var(--color-vue-green200);
+  text-decoration: underline;
+}
+
+.eventcard-content ::v-deep(a:hover) {
+  opacity: 0.4;
+  transition: .2s;
+}
+
+</style>
---
+++ apps/web/app/components/event/EventMultipleAssets.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import type { Event } from '@vuejs-jp/model'
+
+interface EventMultipleAssetsProps {
+  title?: Extract<Event, 'vuejs-handson'>
+}
+
+const props = withDefaults(defineProps<EventMultipleAssetsProps>(), {
+  title: 'vuejs-handson',
+})
+</script>
+
+<template>
+  <div class="eventcard-images">
+    <img :src="`/event/${title}-1.png`" alt="" />
+    <img :src="`/event/${title}-2.png`" alt="" />
+  </div>
+</template>
+
+<style scoped>
+@import url('~/assets/media.css');
+
+.eventcard-images {
+  display: flex;
+  gap: calc(var(--unit) * 1.5);
+
+  @media (--mobile) {
+    flex-direction: column;
+  }
+}
+
+.eventcard-images ::v-deep(img) {
+  --head-img-height: 192px;
+
+  height: var(--head-img-height);
+}
+</style>
