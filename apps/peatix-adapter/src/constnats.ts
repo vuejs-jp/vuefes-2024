98f9a5b99686fd8c34050a5a1050b67d3618aa99
jiyuujin <jiyuujinunite@gmail.com>
2024-07-16 09:08:11 +0900

    feat: ticketName to role

--- apps/peatix-adapter/src/constnats.ts
+++ apps/peatix-adapter/src/constnats.ts
@@ -1,4 +1,10 @@
 export const Constants = {
   PEATIX_LOGIN_URL: 'https://peatix.com/signin',
   PEATIX_DASHBOARD_URL: 'https://peatix.com/event/',
-}
+  PEATIX_GENERAL_TICKET: '一般チケット',
+  PEATIX_WITH_PARTY_TICKET: '一般＋アフターパーティーチケット',
+  PEATIX_GENERAL_ROLE: 'attendee',
+  PEATIX_WITH_PARTY_ROLE: 'attendee + party',
+} as const
+
+export type TicketRole = Extract<typeof Constants[keyof typeof Constants], 'attendee' | 'attendee + party'>
