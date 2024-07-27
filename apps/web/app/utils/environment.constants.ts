0c37faed9d4cf8907f085b39f71c819685b61ceb
jiyuujin <jiyuujinunite@gmail.com>
2024-07-27 19:17:21 +0900

    for preview

--- apps/web/app/utils/environment.constants.ts
+++ apps/web/app/utils/environment.constants.ts
@@ -1,4 +1,4 @@
 export const isProd = process.env.NODE_ENV === 'production'
 
 // for preview
-export const REDIRECT_URL = isProd ? 'https://vuefes.jp/2024' : 'https://localhost:3000'
+export const REDIRECT_URL = isProd ? 'https://vuefes.jp/2024' : 'https://deploy-preview-233--vuefes-2024.netlify.app/2024'
