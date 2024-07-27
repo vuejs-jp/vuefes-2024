feafa6a9de8be675205bff1bf67bd741ea536325
jiyuujin <jiyuujinunite@gmail.com>
2024-07-27 15:01:13 +0900

    fix: model
    - refactor: after-purchase

--- packages/model/lib/ticket.ts
+++ packages/model/lib/ticket.ts
@@ -1,4 +1,6 @@
-export type Ticket = typeof GENERAL_TICKET | typeof WITH_PARTY_TICKET
+export type Ticket = typeof GENERAL_TICKET | typeof WITH_PARTY_TICKET | typeof HANDSON_TICKET | typeof PERSONAL_SPONSOR_TICKET
 
 export const GENERAL_TICKET = '一般チケット'
 export const WITH_PARTY_TICKET = '一般＋アフターパーティーチケット'
+export const HANDSON_TICKET = 'ハンズオンチケット'
+export const PERSONAL_SPONSOR_TICKET = '個人スポンサーチケット'
