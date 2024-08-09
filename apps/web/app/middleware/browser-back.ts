296153d33635ab10d382331e5a730fb695e5ef8f
jiyuujin <jiyuujinunite@gmail.com>
2024-08-09 13:35:48 +0900

    Revert - fix

--- apps/web/app/middleware/browser-back.ts
+++ apps/web/app/middleware/browser-back.ts
@@ -1,7 +1,7 @@
 import { defineNuxtRouteMiddleware, reloadNuxtApp } from '#imports'
 
 export default defineNuxtRouteMiddleware(async (to, from) => {
-  if (to.name && !from.name) {
+  if (to.name === null && from.name !== null) {
     // ブラウザバックが検知された時
     await reloadNuxtApp()
   }
