861bd9cffd9228a687e984a128074dacdd12d56c
jiyuujin <jiyuujinunite@gmail.com>
2024-08-28 22:48:57 +0900

    update staff api

--- apps/web/app/server/api/staffs.get.ts
+++ apps/web/app/server/api/staffs.get.ts
@@ -10,13 +10,13 @@ export default defineEventHandler(async (event) => {
   const { data: _staffs } = await client.from('staffs').select().order('display_order') as { data: Staff[] }
   staffs = _staffs
 
-  const allStaffs: StaffInfo = {
-    type: 'staff',
-    title: 'staff',
+  const coreStaffs: StaffInfo = {
+    type: 'core-staff',
+    title: 'core-staff',
     list: staffs
       .filter((staff: Staff) => {
-        if (process.env.NODE_ENV === 'production') return staff.is_open === true
-        return true
+        if (process.env.NODE_ENV === 'production') return staff.is_open === true && staff.is_volunteer === false
+        return staff.is_volunteer === false
       })
       .sort((a: Staff, b: Staff) => {
         if (a.display_order) return -1
@@ -27,5 +27,22 @@ export default defineEventHandler(async (event) => {
       }),
   }
 
-  return { allStaffs }
+  const volunteerStaffs: StaffInfo = {
+    type: 'volunteer-staff',
+    title: 'volunteer-staff',
+    list: staffs
+      .filter((staff: Staff) => {
+        if (process.env.NODE_ENV === 'production') return staff.is_open === true && staff.is_volunteer === true
+        return staff.is_volunteer === true
+      })
+      .sort((a: Staff, b: Staff) => {
+        if (a.display_order) return -1
+        if (b.display_order) return 1
+        if (!a.display_order) return a.name < b.name ? -1 : 1
+        if (!b.display_order) return a.name < b.name ? -1 : 1
+        return a.display_order - b.display_order
+      }),
+  }
+
+  return { coreStaffs, volunteerStaffs }
 })
